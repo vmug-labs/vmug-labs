5c333897d65975df6c50bfdacbdbcf2d0dd79cdb
Troy Lindsay <tlindsay42@gmail.com>
2018-03-29 22:59:30 -0500

    Improve output variable names

--- live/global/domain_controller/outputs.tf
+++ live/global/domain_controller/outputs.tf
@@ -1,7 +1,7 @@
-output "domain_controller_private_ip" {
+output "private_ip" {
   value = "${aws_instance.domain_controller.private_ip}"
 }
 
-output "domain_controller_base64-encoded_ec2-key-encrypted_password" {
+output "base64-encoded_ec2-key-encrypted_password" {
   value = "${aws_instance.domain_controller.password_data}"
 }
--- live/global/jumphost/outputs.tf
+++ live/global/jumphost/outputs.tf
@@ -1,7 +1,7 @@
-output "jumphost_public_dns" {
+output "public_dns" {
   value = "${aws_instance.jumphost.public_dns}"
 }
 
-output "jumphost_base64-encoded_ec2-key-encrypted_password" {
+output "base64-encoded_ec2-key-encrypted_password" {
   value = "${aws_instance.jumphost.password_data}"
 }
